c75b2976c4f3a8eb8d8b3164b20f75d60df8b5f9
young-46 <pink495sy@gmail.com>
2021-09-20 15:01:34 +0900

    아이스 닉네임 가져오기

--- Assets/Script/A_Script/Central/icecreamButton.cs
+++ Assets/Script/A_Script/Central/icecreamButton.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class icecreamButton : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class icecreamButton : MonoBehaviour
     public string iceKind;
     public GameObject Mint;
     public GameObject Choco;
+    public GameObject icenicknameObject;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,13 +18,26 @@ public class icecreamButton : MonoBehaviour
         Mint = GameObject.Find("mintIcecream");
         Choco = GameObject.Find("ChocoIcecream");
         iceKind = gameObject.GetComponentInChildren<Text>().text;
+
+        icenicknameObject = GameObject.Find("icenicknameObject");
     }
 
     // Update is called once per frame
     public void OnClickConfirmButton(){
-        Mint.SetActive(false);
-        Choco.SetActive(false);
-        nickname.SetActive(true);
-        GameObject.Find("icenicknameObject").GetComponent<nicknameObject>().icekind = iceKind;
+        if(null != GameObject.Find("DataObject")){
+            Mint.SetActive(false);
+            Choco.SetActive(false);
+            nickname.SetActive(true);
+            GameObject.Find("icenicknameObject").GetComponent<nicknameObject>().icekind = iceKind;
+        }
+        else{
+            Mint.SetActive(false);
+            Choco.SetActive(false);
+            //nickname.SetActive(true);
+            icenicknameObject.GetComponent<nicknameObject>().icekind = iceKind;
+            icenicknameObject.GetComponent<nicknameObject>().icenickname = "아이스";
+            DontDestroyOnLoad(icenicknameObject);
+            SceneManager.LoadScene("B");
+        }
     }
 }
---
+++ Assets/Script/B_Script/textChange.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textChange : MonoBehaviour
+{
+    public string nick;
+    public GameObject Destroytext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nick = GameObject.Find("icenicknameObject").GetComponent<nicknameObject>().icenickname;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroytext.transform.Find("Text").gameObject.GetComponent<Text>().text = nick + " 세뇌 중";
+       /* if(nick == "아이스 플레이어"){
+            subtitleClip.subtitleText = "우아아";
+        }
+        else{
+            subtitleClip.subtitleText = "이 더러운" + "몸을 멋대로 움직여주겠어!";
+        }*/
+    }
+}
