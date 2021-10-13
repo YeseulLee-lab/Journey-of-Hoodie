d079ce2d0ac452aa14f8f8494317cca44b7c69f8
young-46 <pink495sy@gmail.com>
2021-10-13 22:41:08 +0900

    멀티 연결 최종 수정

---
+++ Assets/Script/multi/Test.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("테스트 중입니다");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
